5667a6d62c8932d28a272eb7d5ace7b6f7ee17ae
Filip Horvat <fico7489@gmail.com>
2023-02-21 14:38:02 +0100

    initial

--- public/index.php
+++ public/index.php
@@ -1,11 +1,3 @@
 <?php
 
-echo 'test...'. "<br/>";
-
-$processUser = posix_getpwuid(posix_geteuid());
-
-print 'NAME=' . $processUser['name'] . "<br/>";
-print 'UID=' . Posix_geteuid() . "<br/>";
-
-//mkdir('uploads/aaaa');
-
+echo 'test PHP';
